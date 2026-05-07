0e555349831ed1d4408742e1fb683dc451af4498
Rohnicka89 <94117492+Rohnicka89@users.noreply.github.com>
2026-05-07 21:49:27 +0200

    Delete functions directory

--- functions/api/auth/setup-status.js
+++
@@ -1,8 +0,0 @@
-import { json } from '../../_shared/utils.js';
-
-export async function onRequestGet({ env }) {
-  const result = await env.DB
-    .prepare('SELECT COUNT(*) as cnt FROM users')
-    .first();
-  return json({ isSetup: (result?.cnt || 0) > 0 });
-}
